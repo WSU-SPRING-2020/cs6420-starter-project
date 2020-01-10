3c70d5aa79817e61042741f6ac8ae99fbe4269fd
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2020-01-09 18:08:51 -0700

    Initial commit

--- src/test/java/cs6420/sample_app/AppTest.java
+++ src/test/java/cs6420/sample_app/AppTest.java
@@ -3,10 +3,6 @@ package cs6420.sample_app;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-
 class AppTest {
     @Test void appHasAGreeting() {
         assertEquals(new App().getGreeting(), "Hello world.", "Greeting is not correct");
